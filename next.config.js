a00dd39707c76d0e5a83facaba0c222e4d500aab
Mahdi Hasani <mahdi.hasani@chargoon.net>
2019-10-05 10:31:35 +0330

    build serverjs

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
-// module.exports = {
-//     exportPathMap: function () {
-//       return {
-//         '/': { page: '/' },
-//         '/about' :{page:'/about'},
-//         '/project' :{page:'/project'},
-//         '/gallery' :{page:'/gallery'}
-//       }
-//     }
-//   }
+module.exports = {
+    exportPathMap: function () {
+      return {
+        '/': { page: '/' },
+        '/about' : { page:'/about'},
+        '/project' : { page:'/project'},
+        '/gallery' : { page:'/gallery'}
+      }
+    }
+  }
